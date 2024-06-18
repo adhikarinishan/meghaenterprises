8fc984d8a1a4f85f5549d024f6b350d78b4b757e
adhikarinishan <adhikarinishan786@gmail.com>
2024-06-18 16:46:09 +0530

    added list of work done_osig society

--- src/app/Shared/data-service.service.ts
+++ src/app/Shared/data-service.service.ts
@@ -107,6 +107,13 @@ export class DataService {
     'Roopmilan Co.Op Housing Society Ltd., Opp. Venus Cinema, Ulhasnagar - 4',
     'Shobhana Co.Op Housing Society Ltd., Sai section, Ambernath (E)',
     'Sai Digambar Co.Op Housing Society Ltd., Behind Holy Cross School, Nandivali, Kalyan (E)',
+    'Shivam Co. Op. Housing Society Ltd., Kansai Section, Ambernath (E)',
+    'Kapleshwar Co. Op. Housing Society Ltd., Kailash Colony Chowk, Near Hanuman Mandir, Ulhasnagar -5',
+    'Manik Sadan Co. Op. Housing Society Ltd., Kurla Camp, Ulhasnagar-4',
+    'Malhar Park Co. Op. Housing Societies Association Ltd., Mauje-Katemanivali, Chinchpada Rd., Near Khadan, Kalyan (E)',
+    'Skyline Co. Op. Housing Society Ltd., Pune Link Rd., Nitin Raj Bar, Katemanivali, Kalyan (E)',
+    'Unity Co. Op. Housing Society Ltd., Katemanivali, Nana Pavshe Chowk, Kalyan (E)',
+    'New Vishal Sagar Co. Op. Housing Society Ltd., Katemanivali, Near Hanuman Mandir, Kalyan (E)',
   ];
 
   updateDevice(status: boolean) {
